73c70225a65dac0395068912852ac6b19b715cbd
ORGinalBIbEK <bibekmhrzn23@gmail.com>
2026-07-06 21:57:12 +0545

    added a default speed

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
+var speed=500
 func _ready():
 	position=Vector2(100,500)
 
 func _process(delta):
 	var direction=Input.get_vector("left","right","up","down")
-	position+= direction *500 * delta
+	position+= direction *speed * delta
