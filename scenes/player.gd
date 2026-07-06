6096a897a0decebf802b09e92eee9bcd7e4ec7d9
ORGinalBIbEK <bibekmhrzn23@gmail.com>
2026-07-06 21:58:25 +0545

    default speed is set to interger

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
-var speed=500
+@export var speed: int=500
 func _ready():
 	position=Vector2(100,500)
 
 func _process(delta):
 	var direction=Input.get_vector("left","right","up","down")
 	position+= direction *speed * delta
+ 
