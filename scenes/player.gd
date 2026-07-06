77accd194921bbfb2006d622866468aef5f9611a
ORGinalBIbEK <bibekmhrzn23@gmail.com>
2026-07-06 11:32:50 +0545

    added some keys

--- scenes/player.gd
+++ scenes/player.gd
@@ -4,7 +4,7 @@ func _ready():
 	position=Vector2(100,500)
 
 func _process(delta):
-	print(Input.is_action_pressed("ui_down"))
-	position += Vector2(1,0) * 50 * delta
-	$PlayerImage.rotation +=0.1*delta
+	if Input.is_action_pressed("ui_down"):
+		position += Vector2(1,0) * 50 * delta
+		$PlayerImage.rotation +=0.1*delta
 	
