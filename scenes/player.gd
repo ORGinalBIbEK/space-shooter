f2e169e78895df56a20b7752571cfdf7101354c0
ORGinalBIbEK <bibekmhrzn23@gmail.com>
2026-07-06 10:40:07 +0545

    is_action_button

--- scenes/player.gd
+++ scenes/player.gd
@@ -4,6 +4,7 @@ func _ready():
 	position=Vector2(100,500)
 
 func _process(delta):
+	print(Input.is_action_pressed("ui_down"))
 	position += Vector2(1,0) * 50 * delta
 	$PlayerImage.rotation +=0.1*delta
-	$PlayerImage2.rotation+=0.1*delta
+	
