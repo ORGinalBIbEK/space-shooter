06ed17719189216e3ca19e2f6b5eade7b05e2180
ORGinalBIbEK <bibekmhrzn23@gmail.com>
2026-07-07 21:01:07 +0545

    setting up meteors

--- scenes/level.gd
+++ scenes/level.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
-
-func _on_timer_timeout() -> void:
-	print('meteor')
-
+#1. load the scene
+var meteor_scene:PackedScene=load("res://scenes/meteor.tscn")
 
 func _on_meteor_timer_timeout() -> void:
-	pass # Replace with function body.
+	#2. create an instance
+	var meteor = meteor_scene.instantiate()
+	#3. attach the node to the scene tree
+	$Meteros.add_child(meteor)
