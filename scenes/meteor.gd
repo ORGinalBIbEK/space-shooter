7cbd096f003702258963f07462c87be5a968a6e9
ORGinalBIbEK <bibekmhrzn23@gmail.com>
2026-07-07 23:05:33 +0545

    get.viewpoint

--- scenes/meteor.gd
+++ scenes/meteor.gd
@@ -1,6 +1,10 @@
 extends Area2D
 
-
+func _ready():
+	var rng:= RandomNumberGenerator.new()
+	#start position
+	var width=get_viewport().get_visible_rect().size
+	
 func _on_body_entered(body) :
 	print('body entered')
 	
