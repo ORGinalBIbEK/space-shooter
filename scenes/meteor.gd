8c09df774319ea3f5bb98036506ad4a5d494e06a
ORGinalBIbEK <bibekmhrzn23@gmail.com>
2026-07-07 23:46:41 +0545

    some edit in meteor

--- scenes/meteor.gd
+++ scenes/meteor.gd
@@ -5,7 +5,11 @@ func _ready():
 	#start position
 	var width=get_viewport().get_visible_rect().size[0]
 	var random_x=rng.randi_range(0,width)
-	print(random_x)
+	var random_y=rng.randi_range(-150,-50)
+	position =Vector2(random_x,random_y)
+	
+func _process(delta):
+	position+=Vector2(0,1.0)*400*delta
 	
 func _on_body_entered(body) :
 	print('body entered')
