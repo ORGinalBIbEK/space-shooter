7fb896f6e3865b84ad706a7a83025e91d3f36b1d
ORGinalBIbEK <bibekmhrzn23@gmail.com>
2026-07-07 23:31:13 +0545

    random generator meteor

--- scenes/meteor.gd
+++ scenes/meteor.gd
@@ -3,7 +3,9 @@ extends Area2D
 func _ready():
 	var rng:= RandomNumberGenerator.new()
 	#start position
-	var width=get_viewport().get_visible_rect().size
+	var width=get_viewport().get_visible_rect().size[0]
+	var random_x=rng.randi_range(0,width)
+	print(random_x)
 	
 func _on_body_entered(body) :
 	print('body entered')
