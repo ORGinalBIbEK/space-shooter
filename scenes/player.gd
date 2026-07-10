2c2a782ce5da276e025c0b9fc131d23bd3ec1bb5
ORGinalBIbEK <bibekmhrzn23@gmail.com>
2026-07-10 22:45:01 +0545

    some changes

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,6 +1,9 @@
 extends CharacterBody2D 
 
 @export var speed: int=500
+
+signal laser
+
 func _ready():
 	position=Vector2(100,500)
 
@@ -8,3 +11,5 @@ func _process(delta):
 	var direction=Input.get_vector("left","right","up","down")
 	velocity=direction *speed 
 	move_and_slide() 
+	
+	#shoot input
