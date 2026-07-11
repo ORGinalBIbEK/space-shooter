4d10aee0c55717abdc559984bba1e21032fbc3ee
ORGinalBIbEK <bibekmhrzn23@gmail.com>
2026-07-11 23:58:48 +0545

    3 life and gameover

--- scenes/level.gd
+++ scenes/level.gd
@@ -6,6 +6,10 @@ var laser_scene: PackedScene=load("res://laser.tscn")
 
 var health :int =3
 
+func _ready():
+	get_tree().call_group('ui','set_health',health)
+
+
 func _on_meteor_timer_timeout() -> void:
 	#2. create an instance
 	var meteor = meteor_scene.instantiate()
@@ -18,9 +22,9 @@ func _on_meteor_timer_timeout() -> void:
 func on_meteor_collision():
 	health-=1
 	get_tree().call_group('ui','set_health',health)
-	if health<=0:
-		print("dead")
 
+	if health<=0:
+		get_tree().change_scene_to_file("res://scenes/game_over.tscn")
 func _on_player_laser(pos) -> void:
 	var laser=laser_scene.instantiate()
 	$Lasers.add_child(laser)
