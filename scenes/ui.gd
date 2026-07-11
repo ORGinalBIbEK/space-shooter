65a4bc2342169e7bcc0ccf1a1deca990fb58ab3d
ORGinalBIbEK <bibekmhrzn23@gmail.com>
2026-07-11 23:53:38 +0545

    succesffuly addeed player health

--- scenes/ui.gd
+++ scenes/ui.gd
@@ -1,8 +1,12 @@
 extends CanvasLayer
 
+var image=load("res://PNG/playerShip1_red.png")
 func set_health(amount):
 #remove the children
 	for child in $MarginContainer2/HBoxContainer.get_children():
-		
+		child.queue_free()
 #create new children is set by health
-	
+	for i in amount:
+		var text_rect=TextureRect.new()
+		text_rect.texture=image
+		$MarginContainer2/HBoxContainer.add_child(text_rect)
