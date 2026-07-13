aa411910c0d5ea1e509057c57200ade5981a7bc8
ORGinalBIbEK <bibekmhrzn23@gmail.com>
2026-07-13 20:17:58 +0545

    adding stop timer

--- scenes/ui.gd
+++ scenes/ui.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 var image=load("res://PNG/playerShip1_red.png")
+var time_elapsed: =0
 func set_health(amount):
 #remove the children
 	for child in $MarginContainer2/HBoxContainer.get_children():
@@ -10,3 +11,8 @@ func set_health(amount):
 		var text_rect=TextureRect.new()
 		text_rect.texture=image
 		$MarginContainer2/HBoxContainer.add_child(text_rect)
+
+
+func _on_score_timer_timeout() -> void:
+	time_elapsed+=1
+	$MarginContainer/Label.text=str (time_elapsed)
