3d32f01f18a608022888010879d614c51f07292e
ORGinalBIbEK <bibekmhrzn23@gmail.com>
2026-07-13 20:06:47 +0545

    setting up timer

---
+++ scenes/game_over.gd
@@ -0,0 +1,6 @@
+extends Control
+
+@export 	var level_scene:PackedScene
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("shoot"):
+		get_tree().change_scene_to_packed(level_scene)
