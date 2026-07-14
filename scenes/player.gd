d2ec1df989326867566e77505b5f32c90aa9941e
ORGinalBIbEK <bibekmhrzn23@gmail.com>
2026-07-14 22:27:53 +0545

    game finished

--- scenes/player.gd
+++ scenes/player.gd
@@ -7,19 +7,16 @@ signal laser(position)
 
 func _ready():
 	position=Vector2(100,500)
-
 func _process(_delta):
 	var direction=Input.get_vector("left","right","up","down")
 	velocity=direction *speed 
 	move_and_slide() 
-	
 	#shoot input
 	if Input.is_action_just_pressed("shoot") and can_shoot:
 		laser.emit($LaserStartPosition.global_position)
 		can_shoot=false
 		$LaserTimer.start()
 		$AudioStreamPlayer2D.play()
-
 
 func _on_laser_timer_timeout() -> void:
 	can_shoot=true
